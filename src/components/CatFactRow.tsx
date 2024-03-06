74635c76f3818297319b994bd6e39e01d43deb32
Przemyslaw Musialik <przemyslaw.musialik@intive.com>
2024-03-06 21:21:25 +0100

    Extract CatFactRow and improve

---
+++ src/components/CatFactRow.tsx
@@ -0,0 +1,51 @@
+import { useCallback } from 'react';
+import { CatFact } from '../types/CatFact.type';
+import { addFavorite } from '../utils/storage-favorites';
+import { useToast } from './ui/use-toast';
+import { useCatFactsContext } from '../hooks/useCatFactsContext';
+import { Star, Trash2 } from 'lucide-react';
+
+interface Props {
+  fact: CatFact;
+}
+
+export const CatFactRow = ({ fact }: Props) => {
+  const { fetchCatFact, removeCatFact } = useCatFactsContext();
+  const { toast } = useToast();
+
+  const handleRemove = useCallback(() => {
+    removeCatFact(fact._id);
+    fetchCatFact();
+
+    toast({
+      title: 'Success!',
+      description: 'Cat fact removed from favorites.',
+    });
+  }, [fact._id, removeCatFact, fetchCatFact, toast]);
+
+  const handleAddToFavorites = useCallback(() => {
+    addFavorite(fact);
+    handleRemove();
+
+    toast({
+      title: 'Success!',
+      description: 'Cat fact added to favorites.',
+    });
+  }, [fact, addFavorite, fetchCatFact, toast]);
+
+  return (
+    <div className="flex items-center justify-between border-b py-3">
+      <p className="flex-1" key={fact._id}>
+        {fact.text}
+      </p>
+      <div>
+        <button className="ml-1" onClick={handleAddToFavorites}>
+          <Star strokeWidth={1.5} size={16} />
+        </button>
+        <button className="ml-1" onClick={handleRemove}>
+          <Trash2 strokeWidth={1.5} size={16} />
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/components/CatFactsCard.tsx
+++ src/components/CatFactsCard.tsx
@@ -1,32 +1,11 @@
 import { Card, CardContent, CardFooter } from './ui/card';
 import { Button } from './ui/button';
-import { useCallback } from 'react';
-import { CatFact } from '../types/CatFact.type';
-import { addFavorite } from '../utils/storage-favorites';
-import { useToast } from './ui/use-toast';
 import { useCatFactsContext } from '../hooks/useCatFactsContext';
-import { Star, Trash2 } from 'lucide-react';
+
+import { CatFactRow } from './CatFactRow';
 
 export const CatFactsCard = () => {
-  const { catFacts, isLoading, isError, fetchCatFact, removeCatFact } = useCatFactsContext();
-  const { toast } = useToast();
-
-  const addToFavorites = (fact: CatFact) => {
-    addFavorite(fact);
-
-    toast({
-      title: 'Success!',
-      description: 'Cat fact added to favorites.',
-    });
-  };
-
-  const handleAddToFavorites = useCallback(
-    (catFact: CatFact) => {
-      addToFavorites(catFact);
-      fetchCatFact();
-    },
-    [addToFavorites, fetchCatFact],
-  );
+  const { catFacts, isLoading, isError, fetchCatFact } = useCatFactsContext();
 
   return (
     <Card>
@@ -35,19 +14,7 @@ export const CatFactsCard = () => {
         {catFacts.length === 0 && isError && <p>Sorry, something went wrong!</p>}
 
         {catFacts.map((fact) => (
-          <div key={fact._id} className="flex items-center justify-between border-b py-3">
-            <p className="flex-1" key={fact._id}>
-              {fact.text}
-            </p>
-            <div>
-              <button className="ml-1" onClick={() => handleAddToFavorites(fact)}>
-                <Star strokeWidth={1.5} size={16} />
-              </button>
-              <button className="ml-1" onClick={() => removeCatFact(fact._id)}>
-                <Trash2 strokeWidth={1.5} size={16} />
-              </button>
-            </div>
-          </div>
+          <CatFactRow key={fact._id} fact={fact} />
         ))}
       </CardContent>
       <CardFooter>
