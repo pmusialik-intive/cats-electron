a644f5ba8e8d37a874849de0ff3a549ab787fad8
Przemyslaw Musialik <przemyslaw.musialik@intive.com>
2024-03-06 10:19:21 +0100

    Add 10 seconds frequency option

--- src/components/SettingsCard.tsx
+++ src/components/SettingsCard.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
 import { Button } from './ui/button';
-import { HOUR, MINUTE } from '../constants/time';
+import { HOUR, MINUTE, SECOND } from '../constants/time';
 import {
   Select,
   SelectContent,
@@ -11,6 +11,7 @@ import {
 } from './ui/select';
 
 const frequencyOptions = [
+  { label: '10 seconds', value: `${10 * SECOND}` },
   { label: '5 minutes', value: `${5 * MINUTE}` },
   { label: '10 minutes', value: `${10 * MINUTE}` },
   { label: '30 minutes', value: `${30 * MINUTE}` },
