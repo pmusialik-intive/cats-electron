1eb01acc790e221b598dde3f96cc91a32716d312
Przemyslaw Musialik <przemyslaw.musialik@intive.com>
2024-03-06 10:17:50 +0100

    Add shouldFetchNewFact logic

--- src/components/DailyFactCard.tsx
+++ src/components/DailyFactCard.tsx
@@ -1,14 +1,42 @@
 import { useState, useEffect } from 'react';
 import { getRandomCatFact } from '../api/getRandomCatFact';
 import { Card, CardContent } from './ui/card';
-import { LAST_CAT_FACT } from '../constants/local-storage';
+import {
+  LAST_CAT_FACT,
+  LAST_CAT_FACT_TIMESTAMP,
+  NOTIFICATION_FREQUENCY,
+} from '../constants/local-storage';
+import { HOUR } from '../constants/time';
 
 export const DailyFactCard = () => {
   const [catFactText, setCatFactText] = useState('');
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [shouldFetchNewFact, setShouldFetchNewFact] = useState(false);
 
   useEffect(() => {
+    const lastTimestamp = localStorage.getItem(LAST_CAT_FACT_TIMESTAMP);
+    const lastCatFact = localStorage.getItem(LAST_CAT_FACT);
+
+    if (!lastCatFact || !lastTimestamp) {
+      setShouldFetchNewFact(true);
+      return;
+    }
+
+    const now = new Date().getTime();
+    const lastTimestampNumber = +lastTimestamp;
+    const notificationFrequency = localStorage.getItem(NOTIFICATION_FREQUENCY);
+    const notificationFrequencyNumber = notificationFrequency ? +notificationFrequency : HOUR;
+    const shouldFetchNewFact = now - lastTimestampNumber > notificationFrequencyNumber;
+
+    setShouldFetchNewFact(shouldFetchNewFact);
+  }, []);
+
+  useEffect(() => {
+    if (!shouldFetchNewFact) {
+      return;
+    }
+
     const fetchData = async () => {
       try {
         const fact = await getRandomCatFact();
@@ -16,11 +44,11 @@ export const DailyFactCard = () => {
         setIsError(false);
         setCatFactText(fact.text);
 
-        const storageObject = {
-          id: fact._id, 
+        const storageFact = {
+          id: fact._id,
           timestamp: new Date().getTime(),
         };
-        localStorage.setItem(LAST_CAT_FACT, JSON.stringify(storageObject));
+        localStorage.setItem(LAST_CAT_FACT, JSON.stringify(storageFact));
       } catch (error) {
         setIsLoading(false);
         setIsError(true);
@@ -30,7 +58,7 @@ export const DailyFactCard = () => {
 
     setIsLoading(true);
     fetchData();
-  }, []);
+  }, [shouldFetchNewFact]);
 
   return (
     <Card>
--- src/constants/local-storage.ts
+++ src/constants/local-storage.ts
@@ -1,3 +1,4 @@
 export const LAST_CAT_FACT = 'lastCatFact';
+export const LAST_CAT_FACT_TIMESTAMP = 'lastCatFactTimestamp';
 export const FAVORITE_CAT_FACTS = 'favoriteCatFacts';
 export const NOTIFICATION_FREQUENCY = 'notificationFrequency';
