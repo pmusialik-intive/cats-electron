c4f4c868efe12a7088622f87830f1423656bdf01
Przemyslaw Musialik <przemyslaw.musialik@intive.com>
2024-03-06 21:36:36 +0100

    Change frequency options

--- src/constants/frequency.ts
+++ src/constants/frequency.ts
@@ -2,11 +2,12 @@ import { HOUR, MINUTE, SECOND } from '../constants/time';
 
 export const frequencyOptions = [
   { label: '10 seconds', value: 10 * SECOND },
+  { label: '30 seconds', value: 30 * SECOND },
+  { label: '1 minute', value: MINUTE },
   { label: '5 minutes', value: 5 * MINUTE },
   { label: '10 minutes', value: 10 * MINUTE },
   { label: '30 minutes', value: 30 * MINUTE },
   { label: '1 hour', value: 1 * HOUR },
-  { label: '6 hours', value: 6 * HOUR },
 ];
 
 export const defaultFrequencyValue = frequencyOptions[0].value;
