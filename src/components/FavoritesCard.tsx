ffe3aa6bcb2ee6891d7ab836489b670e7054b411
Przemyslaw Musialik <przemyslaw.musialik@intive.com>
2024-03-06 08:50:06 +0100

    Add favorites card component

---
+++ src/components/FavoritesCard.tsx
@@ -0,0 +1,12 @@
+import { Card, CardContent } from './ui/card';
+
+export const FavoritesCard = () => {
+  return (
+    <Card>
+      <CardContent className="space-y-2 p-5">
+        <p>Favorite 1</p>
+        <p>Favorite 2</p>
+      </CardContent>
+    </Card>
+  );
+};
