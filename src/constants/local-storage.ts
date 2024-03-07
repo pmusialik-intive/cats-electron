7f56feb5daee6825651ce78f7b582f2c3c2c56a2
Przemyslaw Musialik <przemyslaw.musialik@intive.com>
2024-03-07 17:41:06 +0100

    Refactor

--- src/constants/local-storage.ts
+++
@@ -1,5 +0,0 @@
-export const LAST_CAT_FACT_TIMESTAMP = 'lastCatFactTimestamp';
-export const FAVORITE_CAT_FACTS = 'favoriteCatFacts';
-export const NOTIFICATION_FREQUENCY = 'notificationFrequency';
-export const FETCHING_FREQUENCY = 'fetchingFrequency';
-export const NEW_FACTS_NOTIFICATION_FACT_TIMESTAMP = 'newFactsNotificationFactTimestamp';
---
+++ src/constants/storage-key.ts
@@ -0,0 +1,8 @@
+export const STORAGE_KEY = {
+  lastCatFactTimestamp: 'lastCatFactTimestamp',
+  favoriteCatFacts: 'favoriteCatFacts',
+  notificationFrequency: 'notificationFrequency',
+  fetchingFrequency: 'fetchingFrequency',
+  newFactsNotificationFactTimestamp: 'newFactsNotificationFactTimestamp',
+  notifiedFactdIds: 'notifiedFactdIds',
+} as const;
