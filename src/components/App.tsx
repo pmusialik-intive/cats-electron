9cc98df750612479c31e643dc0360052d3838a2b
Przemyslaw Musialik <przemyslaw.musialik@intive.com>
2024-03-07 23:18:57 +0100

    Add max-w-screen-xl

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,7 @@ import { Toaster } from './ui/toaster';
 export const App = () => {
   return (
     <AppProviders>
-      <main className="container mx-auto">
+      <main className="container mx-auto max-w-screen-xl">
         <AppTabs />
       </main>
       <Toaster />
