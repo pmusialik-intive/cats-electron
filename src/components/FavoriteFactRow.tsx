cdbbb2aa826b82d8042399c056307d4f0a13723d
Przemyslaw Musialik <przemyslaw.musialik@intive.com>
2024-03-08 00:13:27 +0100

    Make list items focusable and icons accessible

--- src/components/FavoriteFactRow.tsx
+++ src/components/FavoriteFactRow.tsx
@@ -10,11 +10,12 @@ export const FavoriteFactRow = ({ fact, onRemove }: Props) => {
   return (
     <div
       className={`group flex items-center justify-between py-3 border-b last:pb-0 last:border-0`}
+      tabIndex={0}
     >
       <p className="flex-1 line-clamp-5" key={fact._id}>
         {fact.text}
       </p>
-      <div className="invisible group-hover:visible">
+      <div className="invisible group-hover:visible group-focus-within:visible">
         <button className="ml-1" onClick={() => onRemove(fact._id)}>
           <Trash2 strokeWidth={1.5} size={16} />
         </button>
