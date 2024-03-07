367e6c99e6c32fd18c79abf77547d32f495404d4
Przemyslaw Musialik <przemyslaw.musialik@intive.com>
2024-03-07 01:00:26 +0100

    Implement frequencies tests

---
+++ src/utils/fetching-frequency.test.ts
@@ -0,0 +1,59 @@
+import { defaultFetchingFrequencyValue, fetchingFrequencyOptions } from '../constants/frequency';
+import { FETCHING_FREQUENCY } from '../constants/local-storage';
+import { localStorageMock } from '../test-utils/localStorageMock';
+import {
+  getStoredFetchingFrequency,
+  isValidFetchingFrequency,
+  storeFetchingFrequency,
+} from './fetching-frequency';
+
+describe('isValidFetchingFrequency', () => {
+  it('returns true for a valid frequency', () => {
+    expect(isValidFetchingFrequency(fetchingFrequencyOptions[0].value)).toBeTruthy();
+  });
+
+  it('returns false for an invalid frequency', () => {
+    const invalidFrequency999 = 999;
+    const invalidFrequency0 = 0;
+
+    expect(isValidFetchingFrequency(invalidFrequency999)).toBeFalsy();
+    expect(isValidFetchingFrequency(invalidFrequency0)).toBeFalsy();
+  });
+});
+
+describe('getStoredFetchingFrequency', () => {
+  beforeEach(() => {
+    localStorageMock.clear();
+  });
+
+  it('returns default value if nothing is stored', () => {
+    expect(getStoredFetchingFrequency()).toBe(defaultFetchingFrequencyValue);
+  });
+
+  it('returns stored value if it is valid', () => {
+    const validFrequency = fetchingFrequencyOptions[0].value;
+    localStorageMock.setItem(FETCHING_FREQUENCY, `${validFrequency}`);
+    expect(getStoredFetchingFrequency()).toBe(validFrequency);
+  });
+});
+
+describe('storeFetchingFrequency', () => {
+  beforeEach(() => {
+    localStorageMock.clear();
+  });
+
+  it('stores a valid frequency', () => {
+    const validFrequency = fetchingFrequencyOptions[0].value;
+    storeFetchingFrequency(validFrequency);
+    expect(localStorageMock.setItem).toHaveBeenCalledWith(FETCHING_FREQUENCY, `${validFrequency}`);
+  });
+
+  it('does not store an invalid frequency', () => {
+    const invalidFrequency = 999;
+    storeFetchingFrequency(invalidFrequency);
+    expect(localStorageMock.setItem).not.toHaveBeenCalledWith(
+      FETCHING_FREQUENCY,
+      `${invalidFrequency}`,
+    );
+  });
+});
---
+++ src/utils/notification-frequency.test.ts
@@ -0,0 +1,65 @@
+import {
+  defaultNotificationFrequencyValue,
+  notificationFrequencyOptions,
+} from '../constants/frequency';
+import { NOTIFICATION_FREQUENCY } from '../constants/local-storage';
+import { localStorageMock } from '../test-utils/localStorageMock';
+import {
+  getStoredNotificationFrequency,
+  isValidNotificationFrequency,
+  storeNotificationFrequency,
+} from './notification-frequency';
+
+describe('isValidNotificationFrequency', () => {
+  it('returns true for a valid frequency', () => {
+    expect(isValidNotificationFrequency(notificationFrequencyOptions[0].value)).toBeTruthy();
+  });
+
+  it('returns false for an invalid frequency', () => {
+    const invalidFrequency999 = 999;
+    const invalidFrequency0 = 0;
+
+    expect(isValidNotificationFrequency(invalidFrequency999)).toBeFalsy();
+    expect(isValidNotificationFrequency(invalidFrequency0)).toBeFalsy();
+  });
+});
+
+describe('getStoredNotificationFrequency', () => {
+  beforeEach(() => {
+    localStorageMock.clear();
+  });
+
+  it('returns default value if nothing is stored', () => {
+    expect(getStoredNotificationFrequency()).toBe(defaultNotificationFrequencyValue);
+  });
+
+  it('returns stored value if it is valid', () => {
+    const validFrequency = notificationFrequencyOptions[0].value;
+    localStorageMock.setItem(NOTIFICATION_FREQUENCY, `${validFrequency}`);
+    expect(getStoredNotificationFrequency()).toBe(validFrequency);
+  });
+});
+
+describe('storeNotificationFrequency', () => {
+  beforeEach(() => {
+    localStorageMock.clear();
+  });
+
+  it('stores a valid frequency', () => {
+    const validFrequency = notificationFrequencyOptions[0].value;
+    storeNotificationFrequency(validFrequency);
+    expect(localStorageMock.setItem).toHaveBeenCalledWith(
+      NOTIFICATION_FREQUENCY,
+      `${validFrequency}`,
+    );
+  });
+
+  it('does not store an invalid frequency', () => {
+    const invalidFrequency = 999;
+    storeNotificationFrequency(invalidFrequency);
+    expect(localStorageMock.setItem).not.toHaveBeenCalledWith(
+      NOTIFICATION_FREQUENCY,
+      `${invalidFrequency}`,
+    );
+  });
+});
