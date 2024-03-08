d04c768b2e3312a9ae30219e56676fdd40cb8d2c
Przemyslaw Musialik <przemyslaw.musialik@intive.com>
2024-03-08 11:13:08 +0100

    Use storage utils

--- src/utils/local-storage/notification-frequency.ts
+++ src/utils/local-storage/notification-frequency.ts
@@ -3,12 +3,13 @@ import {
   notificationFrequencyOptions,
 } from '../../constants/notification-frequency';
 import { STORAGE_KEY } from '../../constants/storage-key';
+import { getStorageNumber, storeNumber } from './local-storage';
 
 export const isValidNotificationFrequency = (value: number) =>
   notificationFrequencyOptions.some((option) => option.value === +value);
 
 export const getStoredNotificationFrequency = () => {
-  const storedNotificationFrequency = +localStorage.getItem(STORAGE_KEY.notificationFrequency);
+  const storedNotificationFrequency = getStorageNumber(STORAGE_KEY.notificationFrequency);
 
   return isValidNotificationFrequency(storedNotificationFrequency)
     ? storedNotificationFrequency
@@ -19,6 +20,6 @@ export const storeNotificationFrequency = (value: unknown) => {
   const numberValue = +value;
 
   if (isValidNotificationFrequency(numberValue)) {
-    localStorage.setItem(STORAGE_KEY.notificationFrequency, `${numberValue}`);
+    storeNumber(STORAGE_KEY.notificationFrequency, numberValue);
   }
 };
