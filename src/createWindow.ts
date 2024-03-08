36da440b569fff6c1df6e53ecfcdad4ce7c1dc52
Przemyslaw Musialik <przemyslaw.musialik@intive.com>
2024-03-08 09:46:15 +0100

    Change create window width

--- src/createWindow.ts
+++ src/createWindow.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 export const createWindow = () => {
   const mainWindow = new BrowserWindow({
-    width: 1200,
+    width: 1000,
     height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
