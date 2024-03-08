bdb209585a8f0b513bee4734c33c93bb6de825ab
Przemyslaw Musialik <przemyslaw.musialik@intive.com>
2024-03-08 13:37:42 +0100

    Use new storage utils

--- src/utils/local-storage/fetching-frequency.ts
+++ src/utils/local-storage/fetching-frequency.ts
@@ -3,12 +3,13 @@ import {
   fetchingFrequencyOptions,
 } from '../../constants/fetching-frequency';
 import { STORAGE_KEY } from '../../constants/storage-key';
+import { getStorageNumber, storeNumber } from './local-storage';
 
 export const isValidFetchingFrequency = (value: number) =>
   fetchingFrequencyOptions.some((option) => option.value === +value);
 
 export const getStoredFetchingFrequency = () => {
-  const storedFetchingFrequency = +localStorage.getItem(STORAGE_KEY.fetchingFrequency);
+  const storedFetchingFrequency = getStorageNumber(STORAGE_KEY.fetchingFrequency);
 
   return isValidFetchingFrequency(storedFetchingFrequency)
     ? storedFetchingFrequency
@@ -19,6 +20,6 @@ export const storeFetchingFrequency = (value: unknown) => {
   const numberValue = +value;
 
   if (isValidFetchingFrequency(numberValue)) {
-    localStorage.setItem(STORAGE_KEY.fetchingFrequency, `${numberValue}`);
+    storeNumber(STORAGE_KEY.fetchingFrequency, numberValue);
   }
 };
